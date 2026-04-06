aaa83bc6af67115627f66dd3b67f88ed2b08189b
virgil-ferolino <gitMaleFerolino@gmail.com>
2026-04-06 16:02:48 +0800

    fix: footer

--- src/components/landing-footer/LandingFooter.tsx
+++ src/components/landing-footer/LandingFooter.tsx
@@ -1,9 +1,8 @@
 import FullLogo from "@/assets/FullLogo.webp";
-import { Image } from "@/components/ui";
 const LandingFooter = () => {
   return (
     <footer className="flex w-full h-15 justify-center items-center gap-2 bottom-0">
-      <Image src={FullLogo} alt="full-logo" className="md:w-75 w-55 h-6 md:h-8" />
+      <img src={FullLogo} alt="full-logo" className="md:w-75 w-55 h-6 md:h-8" />
     </footer>
   );
 };
